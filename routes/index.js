8d7ebc245a436ba76fe1e5a10019bcd25c51172c
shahmir811 <shahmirkj@gmail.com>
2017-07-31 09:48:02 +0500

    Outputing Data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var {Product} = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  Product.find().then((docs) => {
+    res.render('shop/index', { title: 'Express', products: docs });
+
+  }).catch((err) => {
+    res.status(404).send(err); //404 - Not Found
+  });
+
 });
 
 module.exports = router;
